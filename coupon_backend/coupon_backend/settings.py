87bbc53f0836b7c623ff52f0f1dcfcf065786720
larriemoses <larriemoses@gmail.com>
2025-05-21 21:42:19 +0100

    modified:   coupon_backend/coupon_backend/settings.py

--- coupon_backend/coupon_backend/settings.py
+++ coupon_backend/coupon_backend/settings.py
@@ -132,6 +132,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
