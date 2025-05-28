24f1731978dfd8e93e5169c6d24d97320ab1b092
larriemoses <larriemoses@gmail.com>
2025-05-28 02:29:28 +0100

    modified:   Coupon_Web/src/components/TopDeals.tsx

--- Coupon_Web/src/components/TopDeals.tsx
+++ Coupon_Web/src/components/TopDeals.tsx
@@ -108,7 +108,7 @@ const TopDeals: React.FC = () => {
   }
 
   return (
-    <div className="p-4 w-[50%] mb:[80%] mx-auto">
+    <div className="p-4 w-[full] mb:[80%] mx-auto">
       {popup && (
         <div className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-green-500 text-white px-6 py-2 rounded shadow-lg z-50 transition-all">
           {popup}
