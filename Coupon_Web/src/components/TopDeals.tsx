b71b119f2ee5b2a09873d8ec620a3a33326110ea
larriemoses <larriemoses@gmail.com>
2025-05-28 02:20:37 +0100

    modified:   Coupon_Web/src/components/TopDeals.tsx

--- Coupon_Web/src/components/TopDeals.tsx
+++ Coupon_Web/src/components/TopDeals.tsx
@@ -116,7 +116,7 @@ const TopDeals: React.FC = () => {
       )}
 
       <h2 className="text-2xl font-bold text-center mb-6">Top Deals</h2>
-      <div className="w-[80%] md:flex "> {/* Changed to grid for layout */}
+      <div className="w-full flex justify-center md:flex "> {/* Changed to grid for layout */}
         {topDealsCoupons.length === 0 && (
           <div className="text-gray-500 text-center py-8 col-span-full">No top deals available.</div>
         )}
