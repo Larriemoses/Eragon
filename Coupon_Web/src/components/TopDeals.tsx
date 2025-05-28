69b279b64f19b51e6cf6aa146b9a2c09d9daa0f0
larriemoses <larriemoses@gmail.com>
2025-05-28 02:49:56 +0100

    modified:   Coupon_Web/src/components/TopDeals.tsx

--- Coupon_Web/src/components/TopDeals.tsx
+++ Coupon_Web/src/components/TopDeals.tsx
@@ -108,7 +108,7 @@ const TopDeals: React.FC = () => {
   }
 
   return (
-    <div className="p-4 w-[50%] mx-auto">
+    <div className="w-full flex flex-col gap-6">
       {popup && (
         <div className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-green-500 text-white px-6 py-2 rounded shadow-lg z-50 transition-all">
           {popup}
@@ -129,7 +129,7 @@ const TopDeals: React.FC = () => {
             <div
               key={coupon.id}
               // Changed to match ProductStore's individual coupon card width: w-full or w-[90%] as it's within max-w-xl
-              className="bg-white rounded-xl shadow-xl p-4 flex flex-col gap-2 relative overflow-hidden" // Removed specific width classes
+              className="bg-gray-100 rounded-xl shadow-xl p-4 flex flex-col gap-2" // Removed specific width classes
             >
               {/* Product Logo */}
               <div className="flex justify-start mb-2">
